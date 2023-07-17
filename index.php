1ce5545c3e59fb97846e3d008b1f07649c0f4361
Bright Elom <97502928+cakezero@users.noreply.github.com>
2023-07-17 17:39:18 +0100

    index.php

--- index.php
+++ index.php
@@ -1,24 +1,59 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>CSC 309 DB Class</title>
 </head>
+
 <body>
     <h2>Registration Form</h2>
 
-    <!-- Select all users and display in a Table -->
-    <table>
+    <table cellpadding="12" cellspacing="2" border="1">
         <thead>
-            <!-- Complete the table header row -->
+            <tr>
+                <td>SN</td>
+                <td>Last Name</td>
+                <td>First Name</td>
+                <td>Gender</td>
+                <td>Date of Birth</td>
+                <td>Email</td>
+            </tr>
         </thead>
 
         <tbody>
             <?php
-                // Complete code to:
-                // Connect to the DB
-                // Select all records from users table
-                // Display all the selected records
+                $server = 'localhost'; // 127.0.0.1
+                $username = 'root';
+                $password = 'rootroot';
+                $db = 'csc309';
+
+                //Server Connection
+                $conn = new mysqli($server, $username, $password, $db);
+            
+            //Check Server Connection
+            if ($conn){
+                //Select Query
+                $sql = "SELECT * FROM users";
+                
+                //Query Result
+                $result = $conn->query($sql);
+                
+                while ($row = $result->fetch_assoc()) {
             ?>
+            <tr>
+                <td><?php echo $row["id"];?></td>
+                <td><?php echo $row["lastname"];?></td>
+                <td><?php echo $row["firstname"];?></td>
+                <td><?php echo $row["gender"];?></td>
+                <td><?php echo $row["date_of_birth"];?></td>
+                <td><?php echo $row["email"];?></td>
+            </tr>
+            <?php 
+                }
+                }
+                // Close Connection
+                $conn->close();
+             ?>
         </tbody>
     </table>
 </body>
